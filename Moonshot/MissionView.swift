dd7863f3b97da1a69a72c8afbe342d68ed87ac5d
v1ncio <airfoil_elector0b@icloud.com>
2021-07-24 04:35:21 +0800

    added astronauts and roles based on the mission on MissionView

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct MissionView: View {
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
     let mission: Mission
+    let astronauts: [CrewMember]
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical) {
@@ -30,11 +35,27 @@ struct MissionView: View {
         .navigationBarTitle(Text(mission.displayName), displayMode: .inline)
 
     }
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+
+        var matches = [CrewMember]()
+
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+
+        self.astronauts = matches
+    }
 }
 
 struct MissionView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     static let missions: [Mission] = Bundle.main.decode("missions.json")
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
     }
 }
