deada97097c6a94e4b5332762101de296866f694
v1ncio <airfoil_elector0b@icloud.com>
2021-07-24 04:06:16 +0800

    Created the main view, added computed properties for the display names, images, and formatted dates

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,12 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Vincio on 7/24/21.
+//
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
